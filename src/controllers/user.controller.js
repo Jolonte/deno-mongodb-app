5fc3c955a4479fd7458c137cf1629e3792fce219
Jolonte <vinicius.nevesco@gmail.com>
2023-11-24 19:58:32 -0300

    feat: User CRUD

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,72 @@ import UserModel from '../models/user.model.js'
 const UserController = {
 	createUser: async (req, res) => {
 		try {
-			console.log(req.body)
 			await UserModel.create(req.body)
-			res.status(201).json({ msg: `Usuario criado com sucesso!` })
+			res.status(201).json({ msg: 'Operação concluída com sucesso!' })
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	getAllUsers: async (req, res) => {
+		try {
+			const users = await UserModel.find()
+			if (!users.length) {
+				res.status(200).json({
+					msg: 'Não existem usuários cadastrados.',
+				})
+				return
+			}
+			res.status(200).json(users)
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	getById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const user = await UserModel.findById(id)
+			if (!user) {
+				res.status(404).json({ msg: 'Usuário não encontrado.' })
+				return
+			}
+			res.status(200).json(user)
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	deleteById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const user = await UserModel.findByIdAndDelete(id)
+			if (!user) {
+				res.status(404).json({ msg: 'Usuário não encontrado.' })
+				return
+			}
+			res.status(200).json({
+				user,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	updateById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const user = await UserModel.findByIdAndUpdate(id, req.body)
+
+			if (!user) {
+				res.status(404).json({ msg: 'Usuário não encontrado.' })
+				return
+			}
+			res.status(200).json({
+				user,
+				msg: 'Operação concluída com sucesso!',
+			})
 		} catch (error) {
 			res.status(500).send(error.message)
 		}
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,8 +3,24 @@ import UserController from '../controllers/user.controller.js'
 
 const UserRouter = express.Router()
 
-UserRouter.route('/users').post((req, res) => {
-	UserController.createUser(req, res)
-})
+UserRouter
+	.route('/users')
+	.post((req, res) => UserController.createUser(req, res))
+
+UserRouter
+	.route('/users')
+	.get((req, res) => UserController.getAllUsers(req, res))
+
+UserRouter
+	.route('/users/:id')
+	.get((req, res) => UserController.getById(req, res))
+
+UserRouter
+	.route('/users/:id')
+	.delete((req, res) => UserController.deleteById(req, res))
+
+UserRouter
+	.route('/users/:id')
+	.put((req, res) => UserController.updateById(req, res))
 
 export default UserRouter
