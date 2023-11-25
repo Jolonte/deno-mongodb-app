db9949f91c68b4c22f13164e539e7da9f3db130d
Jolonte <vinicius.nevesco@gmail.com>
2023-11-25 12:47:57 -0300

    feat: Product CRUD

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -9,6 +9,76 @@ const productController = {
 			res.status(500).send(error.message)
 		}
 	},
+
+	getAllProducts: async (req, res) => {
+		try {
+			const products = await ProductModel.find()
+			if (!products) {
+				res.status(404).json({
+					msg: 'Não existem produtos cadastrados.',
+				})
+				return
+			}
+			res.status(200).json({
+				products,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	getProductById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const product = await ProductModel.findByIdAndUpdate(id, req.body, {
+				new: true,
+			})
+			if (!product) {
+				res.status(404).json({ msg: 'Produto não encontrado.' })
+			}
+			res.status(200).json({
+				product,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	deleteProductById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const product = await ProductModel.findByIdAndDelete(id)
+			if (!product) {
+				res.status(404).send({ msg: 'Produto não encontrado.' })
+			}
+			res.status(200).json({
+				product,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	updateProductById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const product = await ProductModel.findByIdAndUpdate(id, req.body, {
+				new: true,
+			})
+			if (!product) {
+				res.status(404).send({ msg: 'Produto não encontrado.' })
+			}
+			res.status(200).json({
+				product,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
 }
 
 export default productController
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -6,5 +6,12 @@ const ProductRouter = express.Router()
 ProductRouter
 	.route('/products')
 	.post((req, res) => productController.createProduct(req, res))
+	.get((req, res) => productController.getAllProducts(req, res))
+
+ProductRouter
+	.route('/products/:id')
+	.get((req, res) => productController.getProductById(req, res))
+	.delete((req, res) => productController.deleteProductById(req, res))
+	.put((req, res) => productController.updateProductById(req, res))
 
 export default ProductRouter
