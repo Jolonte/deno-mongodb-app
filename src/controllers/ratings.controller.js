b412cbce1b9a714f29bca371b2159e2b7f13aa31
Jolonte <vinicius.nevesco@gmail.com>
2023-11-25 14:20:52 -0300

    feat: Rating Crud

--- src/controllers/ratings.controller.js
+++ src/controllers/ratings.controller.js
@@ -24,6 +24,80 @@ const RatingController = {
 			res.status(500).send(error.message)
 		}
 	},
+
+	getAllRatings: async (req, res) => {
+		try {
+			const rating = await RatingModel.find().populate('userId productId')
+			if (!rating) {
+				res.status(404).json({
+					msg: 'Não existem ratings cadastrados.',
+				})
+				return
+			}
+			res.status(200).json({
+				rating,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
+
+	getRatingById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const rating = await RatingModel.findById(id).populate(
+				'userId productId',
+			)
+			if (!rating) {
+				res.status(404).json({
+					msg: 'Rating não encontrado.',
+				})
+			}
+			res.status(200).json({
+				rating,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(505).send(error.message)
+		}
+	},
+
+	deleteRatingById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const rating = await RatingModel.findByIdAndDelete(id).populate(
+				'userId productId',
+			)
+			if (!rating) {
+				res.status(404).json({ msg: 'Rating não encontrado.' })
+			}
+			res.status(200).json({
+				rating,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(505).send(error.message)
+		}
+	},
+
+	updateRatingById: async (req, res) => {
+		try {
+			const id = req.params.id
+			const rating = await RatingModel.findByIdAndUpdate(id, req.body, {
+				new: true,
+			}).populate('userId productId')
+			if (!rating) {
+				res.status(404).json({ msg: 'Rating não encontrado.' })
+			}
+			res.status(200).json({
+				rating,
+				msg: 'Operação concluída com sucesso!',
+			})
+		} catch (error) {
+			res.status(500).send(error.message)
+		}
+	},
 }
 
 export default RatingController
--- src/routes/ratings.route.js
+++ src/routes/ratings.route.js
@@ -6,8 +6,12 @@ const RatingRouter = express.Router()
 RatingRouter
 	.route('/ratings')
 	.post((req, res) => RatingController.createRating(req, res))
+	.get((req, res) => RatingController.getAllRatings(req, res))
 
 RatingRouter
 	.route('/ratings/:id')
+	.get((req, res) => RatingController.getRatingById(req, res))
+	.delete((req, res) => RatingController.deleteRatingById(req, res))
+	.put((req, res) => RatingController.updateRatingById(req, res))
 
 export default RatingRouter
