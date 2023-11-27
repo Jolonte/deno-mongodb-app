87fee2f437c5afae76a0ff1e43b1757fd26a2b68
Jolonte <vinicius.nevesco@gmail.com>
2023-11-27 17:36:01 -0300

    feat: ClientPreferences CRUD

--- src/routes/clientProductsPreferences.route.js
+++ src/routes/clientProductsPreferences.route.js
@@ -5,13 +5,13 @@ const ClientPreferencesRoute = express.Router()
 
 ClientPreferencesRoute
 	.route('/clientpreferences')
-	.post()
-	.get()
+	.post((req, res) => ClientPreferencesController.addToClientPreferences(req, res))
+	.get((req, res) => ClientPreferencesController.getAllClientPreferences(req, res))
 
 ClientPreferencesRoute
 	.route('/clientpreferences/:id')
-	.get()
-	.delete()
-	.put()
+	.get((req, res) => ClientPreferencesController.getClientPreferencesById(req, res))
+	.delete((req, res) => ClientPreferencesController.deleteClientPreferencesById(req, res))
+	.put((req, res) => ClientPreferencesController.updateClientPreferencesById(req, res))
 
 export default ClientPreferencesRoute
