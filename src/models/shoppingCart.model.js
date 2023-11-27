3a7ae36478f05e838868ae3a216918094096fc2b
Jolonte <vinicius.nevesco@gmail.com>
2023-11-27 18:27:44 -0300

    feat: Product price

--- src/models/shoppingCart.model.js
+++ src/models/shoppingCart.model.js
@@ -1,25 +1,42 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 const shoppingCartSchema = new mongoose.Schema({
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	items: [{
-		productId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Product',
-			required: true,
-		},
-		quantity: {
-			type: Number,
-			required: true,
-			default: 1,
-		},
-	}],
-}, { timestamps: true })
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  items: [{
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+  }],
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+}, { timestamps: true });
 
-const ShoppingCartModel = mongoose.model('ShoppingCart', shoppingCartSchema)
+// Middleware para calcular e atualizar o totalPrice antes de salvar
+shoppingCartSchema.pre('save', async function (next) {
+  const itemPromises = this.items.map(async (item) => {
+    const product = await mongoose.model('Product').findById(item.productId);
+    return item.quantity * (product ? product.productPrice : 0);
+  });
 
-export default ShoppingCartModel
+  const totalPrices = await Promise.all(itemPromises);
+  this.totalPrice = totalPrices.reduce((total, price) => total + price, 0);
+
+  next();
+});
+
+const ShoppingCartModel = mongoose.model('ShoppingCart', shoppingCartSchema);
+
+export default ShoppingCartModel;
