c983a171174ade577a22f546220cbeba2763bf22
Jolonte <vinicius.nevesco@gmail.com>
2023-11-27 17:33:41 -0300

    docs: New comment

--- src/controllers/clientProductsPreferences.controller.js
+++ src/controllers/clientProductsPreferences.controller.js
@@ -83,6 +83,7 @@ const ClientPreferencesController = {
 			const id = req.params.id
 			const { userId } = req.body
 
+            // verificar se o usuário existe no BD
 			const userExists = await UserModel.exists({ _id: userId })
 
 			if (!userExists) {
