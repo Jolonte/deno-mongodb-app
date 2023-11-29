eca2cb1e24ec61cb293ec07aef53860da8b024b0
Jolonte <vinicius.nevesco@gmail.com>
2023-11-28 22:27:19 -0300

    refactor

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,11 +4,11 @@ import AuthController from '../controllers/auth.controller.js'
 const AuthRoute = express.Router()
 
 AuthRoute
-	.route('/login')
-	.post((req, res) => AuthController.loginUserByEmailAndPassword(req, res))
-
-AuthRoute
 	.route('/singup')
 	.post((req, res) => AuthController.singInUser(req, res))
+
+AuthRoute
+	.route('/login')
+	.post((req, res) => AuthController.loginUserByEmailAndPassword(req, res))
 
 export default AuthRoute
