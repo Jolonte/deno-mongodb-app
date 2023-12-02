0ea1b9274d8e0aadb596e3f55ed86869d93a98b9
Jolonte <vinicius.nevesco@gmail.com>
2023-12-01 22:05:56 -0300

    feat: SingIn JWT token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,6 @@
 import UserModel from '../models/user.model.js'
-import { create } from 'https://deno.land/x/djwt@v3.0.1/mod.ts'
+import createToken from '../utils/apiKey.utils.js'
 import { setCookie } from 'https://deno.land/std@0.208.0/http/cookie.ts'
-
-const maxAge = 3 * 24 * 60 * 60 // Value for 3 days
-
-const createToken = (paramId) => {
-	const header = { alg: 'HS256', typ: 'JWT' }
-	const payload = { paramId }
-	const key = Deno.env.get('JWT_TOKEN_SECRET')
-	return create(header, payload, key)
-}
 
 const AuthController = {
 	singInUser: async (req, res) => {
@@ -22,7 +13,7 @@ const AuthController = {
 				value: token,
 				httpOnly: true,
 				secure: true,
-				maxAge: maxAge * 1000,
+				maxAge: 86400,
 			})
 
 			res.status(201).json({
@@ -33,21 +24,39 @@ const AuthController = {
 			res.status(500).send(error.message)
 		}
 	},
-	// loginUserByEmailAndPassword: async (req, res) => {
-	// 	try {
-	// 		const { email, password } = req.body
 
-	// 		if (!email) {
-	// 			res.status(400).send({ msg: 'E-mail vazio.' })
-	// 		} else if (!password) {
-	// 			res.status(400).send({ msg: 'Senha vazia.' })
-	// 		}
+	loginUserByEmailAndPassword: async (req, res) => {
+		// userSchema.statics.login = async function (email, password) {
+		// 	const user = await this.findOne({ email })
+		// 	if (user) {
+		// 		const auth = await bcrypt.compare(password, user.password)
+		// 		if (auth) { // Se a senha estiver correta, retorna o usuário
+		// 			return user
+		// 		}
+		// 		throw Error('Senha incorreta.')
+		// 	}
+		// 	throw Error('E-mail incorreto.')
+		// }
 
-	// 	} catch (error) {
-	// 		console.log(error.message)
-	// 		res.status(500).send(error.message)
-	// 	}
-	// },
+		try {
+			const { email, password } = req.body
+
+			const user = await UserModel.findOne({
+				_id: '656a660034e878e736515532',
+			})
+
+			if (!user) {
+				res.status(404).json('Usuário não encontrado.')
+				return
+			}
+
+			res.status(200).json({ user, msg: 'Operação bem sucedida.' })
+			// const user = await UserModel.login(email, password)
+			// res.status(200).json({ userId: user._id, msg: 'Operação bem sucedida.' })
+		} catch (error) {
+			res.status(400).send(error.message)
+		}
+	},
 }
 
 export default AuthController
